04fe0a099d76bebccecafe938188bad0c3832e70
Pedroj-64 <pedroj.sotor@uqvirtual.edu.co>
2024-10-06 14:05:36 -0500

    -

--- src/test/java/co/edu/uniquindio/poo/EmpleadoTest.java
+++ src/test/java/co/edu/uniquindio/poo/EmpleadoTest.java
@@ -1,16 +1,14 @@
 package co.edu.uniquindio.poo;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.util.logging.*;
-
-import org.junit.jupiter.api.Test;
 
 /**
  * Clase abstracta no tiene metodos definidos ni metodos propios
  */
 public class EmpleadoTest { 
     private Logger LOG=Logger.getLogger(EmpleadoTest.class.getName());
+
+    
     
     
 }
